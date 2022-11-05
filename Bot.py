64c93df94690566845eececf7688f0d528e405a5
Pablo <pablo.cabale@polymtl.ca>
2022-11-05 01:43:55 -0400

    Piano tiles bot fin

--- Bot.py
+++ Bot.py
@@ -5,9 +5,10 @@ import keyboard
 import random
 import win32api, win32con
 
-def click(x,y):
+def click(x,y,pressedTime):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    time.sleep(pressedTime)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
 def displayMouseInfo():
@@ -18,7 +19,21 @@ def displayMouseInfo():
         print(positionStr, end='')
         print('\b' * len(positionStr), end='', flush=True)
 
+def playPianoTiles():
+    position1 = [750, 650]
+    position2 = [875, 650]
+    position3 = [1000, 650]
+    position4 = [1125, 650]
+    positions = [position1, position2, position3, position4]
+    pressedTime = 0.01
+    while not (keyboard.is_pressed('esc')):
+        for position in positions:
+            if pyautogui.pixel(position[0], position[1])[0] == 0:
+                click(position[0], position[1], pressedTime)
+
 def main():
-    displayMouseInfo()
+    playPianoTiles()
 
 main()
+
+#displayMouseInfo()
