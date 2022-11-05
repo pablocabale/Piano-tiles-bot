46ae558ccfd2e34078ea32888d42d37d78847a07
Pablo <pablo.cabale@polymtl.ca>
2022-11-05 00:33:25 -0400

    ajout des librairies

--- Bot.py
+++ Bot.py
@@ -1,1 +1,19 @@
-print("hello world")
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+
+
+def main():
+    click(1000, 500)
+    print("hello world")
+
+main()
