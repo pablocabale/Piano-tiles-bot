d38cb60b82c1c3eeb12699a1eab7834082b1fbe2
Pablo <pablo.cabale@polymtl.ca>
2022-11-05 01:10:30 -0400

    ajout display mouse info

--- Bot.py
+++ Bot.py
@@ -10,10 +10,15 @@ def click(x,y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
-
+def displayMouseInfo():
+    while not (keyboard.is_pressed('esc')):
+        x, y = pyautogui.position()
+        r, g ,b = pyautogui.pixel(x, y)
+        positionStr = 'X: {0} Y: {1}  RGB: ({2}, {3}, {4})'.format(str(x).rjust(4), str(y).rjust(4), str(r).rjust(3), str(g).rjust(3), str(b).rjust(3))
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
 
 def main():
-    click(1000, 500)
-    print("hello world")
+    displayMouseInfo()
 
 main()
